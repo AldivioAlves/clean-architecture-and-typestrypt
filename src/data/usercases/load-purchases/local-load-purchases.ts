404d5265bccacff916bb78ef4446c6886cebe5ce
Aldivio Alves <aldivio89@hotmail.com>
2022-09-28 22:27:15 -0300

    ensure LocalLoadPurchases return an empty list if cache is more than 3 days old

--- src/data/usercases/load-purchases/local-load-purchases.ts
+++ src/data/usercases/load-purchases/local-load-purchases.ts
@@ -2,30 +2,36 @@ import { CacheStore } from "@/data/protocols/cache"
 import { SavePurchases, LoadPurchases } from "@/domains/usecases"
 
 
-export class LocalLoadPurchases implements SavePurchases, LoadPurchases{
+export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
 
     private readonly key = 'purchases'
 
     constructor(
-        private readonly cacheStore:CacheStore,
-        private readonly currentDate:Date
-        ){}
+        private readonly cacheStore: CacheStore,
+        private readonly currentDate: Date
+    ) { }
 
-    async save (purchases:Array<SavePurchases.Params>):Promise<void>{
-        this.cacheStore.replace(this.key,{
-            timestamp: this.currentDate,      
-            value:  purchases
+    async save(purchases: Array<SavePurchases.Params>): Promise<void> {
+        this.cacheStore.replace(this.key, {
+            timestamp: this.currentDate,
+            value: purchases
         })
     }
 
-    async loadAll():Promise<Array<LoadPurchases.Result>>{
-        try{
-           const cache = this.cacheStore.fetch(this.key)
-            return cache.value
-        }catch(error){
+    async loadAll(): Promise<Array<LoadPurchases.Result>> {
+        try {
+            const cache = this.cacheStore.fetch(this.key)
+            const maxAge = new Date(cache.timestamp)
+            maxAge.setDate(maxAge.getDate() + 3)
+            if (maxAge > this.currentDate) {
+                return cache.value
+            } else {
+                throw new Error()
+            }
+        } catch (error) {
             this.cacheStore.delete(this.key)
             return []
         }
-        
+
     }
 }
