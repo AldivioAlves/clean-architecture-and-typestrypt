d561bdb7ec995e35b6a22c9b68d79044ab5170a5
Aldivio Alves <aldivio89@hotmail.com>
2022-10-10 21:43:20 -0300

    remove duplicated code

--- src/data/usercases/load-purchases/local-load-purchases.ts
+++ src/data/usercases/load-purchases/local-load-purchases.ts
@@ -21,27 +21,21 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
     async loadAll(): Promise<Array<LoadPurchases.Result>> {
         try {
             const cache = this.cacheStore.fetch(this.key)
-            if(CachePolicy.validate(cache.timestamp,this.currentDate)){
-                return cache.value
-            } else {
-                return []
-            }
+            return CachePolicy.validate(cache.timestamp, this.currentDate) ? cache.value : [];
         } catch (error) {
             return []
         }
-
-        
     }
 
     validate(): void {
         try {
-          const cache =  this.cacheStore.fetch(this.key)
-            if(!CachePolicy.validate(cache.timestamp,this.currentDate)){
+            const cache = this.cacheStore.fetch(this.key)
+            if (!CachePolicy.validate(cache.timestamp, this.currentDate)) {
                 throw new Error()
             }
         } catch (error) {
             this.cacheStore.delete(this.key)
         }
-        
+
     }
 }
